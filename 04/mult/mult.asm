b4fd6b683d76e98af3a2c80a710903b861f662dc
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-11-25 10:52:29 +0800

    此提交將完成可以作到乘法運算，透過加次數的方式達到
    修改項目:
    
    模組:
    
    議題

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,48 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+
+// int main() {
+//     int R0 = 3;
+@3
+D=A
+@0
+M=D
+
+//     int R1 = 5;
+@5
+D=A
+@1
+M=D
+
+//     int R2 = 0;
+@0
+D=A
+@2
+M=D
+
+//     while (R0 > 0) {
+//       R2 = R2 + R1;
+@1
+D=M
+@2
+M=D+M
+
+//       R0 = R0 - 1;
+@1
+D=A
+@0
+M=M-D
+
+//     }
+@0
+D=M
+@12
+D;JGT
+
+// 鎖定跳躍位置不讓ROM記憶體亂移動
+@24
+0;JMP
+
+// }
